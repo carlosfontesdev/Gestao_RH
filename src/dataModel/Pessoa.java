58e1a49dcdf1f7c7a6c1be4cf4de5b0abaf9bcae
Carlos Fontes <carlos.1999.fontes@gmail.com>
2026-02-11 18:21:02 +0000

    Add files via upload

---
+++ src/dataModel/Pessoa.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dataModel;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author formando
+ */
+public class Pessoa {
+    String nome;
+    String morada; 
+
+    public Pessoa(String nome, String morada) {
+        this.nome = nome;
+        this.morada = morada;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMorada() {
+        return morada;
+    }
+
+    public void setMorada(String morada) {
+        this.morada = morada;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome -> " + nome + "\nMorada -> " + morada ;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pessoa other = (Pessoa) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        return Objects.equals(this.morada, other.morada);
+    }
+    
+}
